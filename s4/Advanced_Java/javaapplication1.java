901c484027392935689e82672df57dcd79962a77
puthusseri <vyshakputhusseri@gmail.com>
2019-03-21 19:39:20 +0530

    Advanced java tutorials

---
+++ s4/Advanced_Java/javaapplication1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Puthusseri
+ */
+import java.util.*;
+
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        List<Integer> n = new ArrayList<>();
+        n.add(5);
+        n.add(3);
+        int i = Integer.parseInt(n.get(1).toString());
+        System.out.println(i);
+
+        
+    }
+    
+}
