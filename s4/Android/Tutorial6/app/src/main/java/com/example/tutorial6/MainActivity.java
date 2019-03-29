ffa1e8641ba52b75232a3347878c0136c43f6ae9
puthusseri <vyshakputhusseri@gmail.com>
2019-03-29 21:08:52 +0530

    auto complete text box

--- s4/Android/Tutorial6/app/src/main/java/com/example/tutorial6/MainActivity.java
+++ s4/Android/Tutorial6/app/src/main/java/com/example/tutorial6/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.tutorial6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] names = {"vyshak puthusseri","vyshnav","yeanest","yedhu thampi","vimuna","vishnu prashanth","Tarson","Tyson"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item,names);
+        AutoCompleteTextView txt = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView);
+        txt.setThreshold(1);//Start prediction from first character
+        txt.setAdapter(adapter);
     }
 }
