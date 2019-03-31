ce289e37527e402d79b26c956ef23e893898a5c8
puthusseri <vyshakputhusseri@gmail.com>
2019-03-31 15:51:46 +0530

    some minor changes only

--- s4/Android/TURORIALS/Tutorial7/app/src/main/java/com/example/tutorial7/MainActivity.java
+++ s4/Android/TURORIALS/Tutorial7/app/src/main/java/com/example/tutorial7/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.tutorial7;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button nextButton;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nextButton = (Button)findViewById(R.id.button);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(R.mipmap.dora);
+            }
+        });
     }
 }
