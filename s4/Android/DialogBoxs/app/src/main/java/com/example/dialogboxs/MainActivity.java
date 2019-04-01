45c0513ed710abe1ec14270a83f26115416a0483
Vyshak Puthusseri <vyshakputhusseri@gmail.com>
2019-04-01 12:59:16 +0530

    PArtial complte of dialog box

--- s4/Android/DialogBoxs/app/src/main/java/com/example/dialogboxs/MainActivity.java
+++ s4/Android/DialogBoxs/app/src/main/java/com/example/dialogboxs/MainActivity.java
@@ -8,12 +8,22 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button age;
+    EditText input;
+    RelativeLayout rl;
+    String msg ="";
+    final CharSequence myList[] = { "Tea", "Coffee", "Milk" };
+    ArrayList<Integer> selList=new ArrayList();
+    boolean bl[] = new boolean[myList.length];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,18 +38,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void  createAlertBox() {
 
         AlertDialog.Builder alertDialog2 = new AlertDialog.Builder(MainActivity.this);
 
-        alertDialog2.setTitle("Confirm Delete...");
-        alertDialog2.setMessage("Are you sure you want delete this file?");
 
+        ArrayList<Integer> selectedItems = new ArrayList<>();
 
+        alertDialog2.setTitle("Name...");
+        alertDialog2.setMessage("Enter the Name");
+
+        alertDialog2.setMultiChoiceItems(myList, bl, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface arg0, int arg1, boolean arg2) {
+                if(arg2)
+                {
+                    // If user select a item then add it in selected items
+                    selList.add(arg1);
+                }
+                else if (selList.contains(arg1))
+                {
+                    // if the item is already selected then remove it
+                    selList.remove(Integer.valueOf(arg1));
+                }
+            }
+
+        });
         alertDialog2.setPositiveButton("YES",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         // Write your code here to execute after dialog
+
+                        msg="";
+                        for (int i = 0; i < selList.size(); i++) {
+
+                            msg=msg+"\n"+(i+1)+" : "+myList[selList.get(i)];
+                        }
+                        Toast.makeText(getApplicationContext(),
+                                "Total "+ selList.size() +" Items Selected.\n"+ msg , Toast.LENGTH_LONG)
+                                .show();
+
+
                         Toast.makeText(getApplicationContext(),
                                 "You clicked on YES", Toast.LENGTH_SHORT)
                                 .show();
@@ -49,6 +89,15 @@ public class MainActivity extends AppCompatActivity {
         alertDialog2.setNegativeButton("NO",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
+
+
+                        // TODO Auto-generated method stub
+                        Toast.makeText(getApplicationContext(),
+                                "You Have Cancel the Dialog box", Toast.LENGTH_LONG)
+                                .show();
+                    
+
+
                         Toast.makeText(getApplicationContext(),
                                 "You clicked on NO", Toast.LENGTH_SHORT)
                                 .show();
