50d160064d3082923f9f6eda47da7aff68971ec4
andlabb <vyshakputhusseri@gmail.com>
2019-04-05 11:32:03 +0530

    Change in the documentation

--- s4/Android/TURORIALS/Tutorial_8/app/src/main/java/com/example/tutorial_8/MainActivity.java
+++ s4/Android/TURORIALS/Tutorial_8/app/src/main/java/com/example/tutorial_8/MainActivity.java
@@ -4,23 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button nextButton;
+    ImageButton nextButton;
     LinearLayout background;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nextButton = (Button)findViewById(R.id.button);
+        nextButton = findViewById(R.id.button);
         background = (LinearLayout)findViewById(R.id.linearLayout);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 background.setBackground(getDrawable(R.drawable.dora));
-
             }
         });
     }
