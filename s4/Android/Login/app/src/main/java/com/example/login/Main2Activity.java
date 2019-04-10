745151e24386ffeb5941840f5bbc58e9115bb2f8
puthusseri <vyshakputhusseri@gmail.com>
2019-04-10 20:59:04 +0530

    Completed the assignment 1

--- s4/Android/Login/app/src/main/java/com/example/login/Main2Activity.java
+++ s4/Android/Login/app/src/main/java/com/example/login/Main2Activity.java
@@ -3,15 +3,13 @@ package com.example.login;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
 public class Main2Activity extends AppCompatActivity {
-
     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         textView = (TextView)findViewById(R.id.display);
-        textView.setText(getIntent().getExtras().getString("username"));
+        textView.setText("User : "+getIntent().getExtras().getString("username"));
     }
 }
--- s4/Android/Login/app/src/main/java/com/example/login/MainActivity.java
+++ s4/Android/Login/app/src/main/java/com/example/login/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.login;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
     EditText password;
     EditText username;
@@ -20,23 +18,18 @@ public class MainActivity extends AppCompatActivity {
         password = (EditText)findViewById(R.id.password);
         username = (EditText)findViewById(R.id.username);
         button = (Button)findViewById(R.id.button);
-        str = username.getText().toString();
-        str2 = password.getText().toString();
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                Intent i = new Intent(getApplicationContext(),Main2Activity.class);
-                i.putExtra("username",str);
-                startActivity(i);
-
-                Toast.makeText(getApplicationContext(),str.toString(),Toast.LENGTH_LONG).show();
-
-               // Toast.makeText(getApplicationContext(),"The username or password is invalid",Toast.LENGTH_LONG).show();
-
-
+                str = username.getText().toString();
+                str2 = password.getText().toString();
+                if(str.equals("admin") && str2.equals("admin")) {
+                    Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                    i.putExtra("username", str);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(getApplicationContext(), str.toString(), Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
