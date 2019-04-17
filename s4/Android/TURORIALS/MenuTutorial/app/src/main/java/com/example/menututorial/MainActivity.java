f53a02fe30217804c78dbb6f16e0f938971c24e4
puthusseri <vyshakputhusseri@gmail.com>
2019-04-17 15:59:39 +0530

    Completed the menu logic

--- s4/Android/TURORIALS/MenuTutorial/app/src/main/java/com/example/menututorial/MainActivity.java
+++ s4/Android/TURORIALS/MenuTutorial/app/src/main/java/com/example/menututorial/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.menututorial;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        constraintLayout = (ConstraintLayout)findViewById(R.id.bck);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -21,12 +28,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public  boolean onOptionsItemSelected(MenuItem menuItem){
         int id = menuItem.getItemId();
-        if(id == R.id.profile){
+        if(id == R.id.red_color){
+            constraintLayout.setBackgroundColor(Color.RED);
             return true;
-
-        }if(id == R.id.a){
+        }
+        if(id == R.id.green_color){
+            constraintLayout.setBackgroundColor(Color.GREEN);
             return true;
-
+        }
+        if(id == R.id.blue_color){
+            constraintLayout.setBackgroundColor(Color.BLUE);
+            return true;
         }
         return  true;
     }
