9773abd23b86394d014a6431ebc9433609344ede
puthusseri <vyshakputhusseri@gmail.com>
2019-04-20 20:54:53 +0530

    SQLITE DATABASE Insertion operation working

---
+++ s4/Android/TURORIALS/SQLite/app/src/main/java/com/example/sqlite/DatabaseHelper.java
@@ -0,0 +1,50 @@
+package com.example.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "SURNAME";
+    public static final String COL_4 = "MARKS";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME,null,1);
+    SQLiteDatabase db = this.getWritableDatabase();
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sql = "CREATE TABLE "+TABLE_NAME+ " ( ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,SURNAME TEXT,MARKS INTEGER)";
+         db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    String sql = "DROP TABLE IF EXISTS "+TABLE_NAME;
+    db.execSQL(sql);
+    onCreate(db);
+    }
+    public boolean insertData(String name, String surname, String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,surname);
+        contentValues.put(COL_3,marks);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+    
+}
+
--- s4/Android/TURORIALS/SQLite/app/src/main/java/com/example/sqlite/MainActivity.java
+++ s4/Android/TURORIALS/SQLite/app/src/main/java/com/example/sqlite/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    EditText editName,editSurname,editMarks;
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+
+        editName = (EditText)findViewById(R.id.editText2);
+        editSurname = (EditText)findViewById(R.id.editText3);
+        editMarks = (EditText)findViewById(R.id.editText5);
+        addButton = (Button)findViewById(R.id.button);
+        AddData();
+    }
+
+    public void AddData() {
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isInserted = myDb.insertData(editName.getText().toString(),
+                        editSurname.getText().toString(),
+                        editMarks.getText().toString());
+                if (isInserted == true)
+                    Toast.makeText(MainActivity.this,"Data inserted ",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this,"Data Not inserted ",Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
