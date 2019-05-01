2f569d6a9b8d5ef77a66bd2cd63494330fc83f92
puthusseri <vyshakputhusseri@gmail.com>
2019-05-01 18:08:30 +0530

    .

--- s4/Android/DimScreen/app/src/main/java/com/example/dimscreen/MainActivity.java
+++ s4/Android/DimScreen/app/src/main/java/com/example/dimscreen/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.dimscreen;
 
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    SeekBar seekBar;
+    Context context;
+    int brightness;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        textView = (TextView)findViewById(R.id.textView);
+        context = getApplicationContext();
     }
+
 }
