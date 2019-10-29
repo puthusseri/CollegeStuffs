e8df2e069ff4321e4e048059e76524d559e48faf
Vyshak Puthusseri <vyshakputhusseri@gmail.com>
2019-10-29 19:26:19 +0530

    feat:Created some required scripts
    These scripts can be used in future to reconfigure my environment for doing
    computer vision problems.

---
+++ installfiles.sh
@@ -0,0 +1,14 @@
+conda install -c menpo dlib
+conda install -c conda-forge tensorflow
+conda install -c conda-forge/label/broken tensorflow
+conda install -c conda-forge/label/cf201901 tensorflow
+conda install -c anaconda keras
+o install this package with conda run one of the following:
+conda install -c conda-forge matplotlib
+conda install -c conda-forge/label/testing matplotlib
+conda install -c conda-forge/label/testing/gcc7 matplotlib
+conda install -c conda-forge/label/gcc7 matplotlib
+conda install -c conda-forge/label/broken matplotlib
+conda install -c conda-forge/label/rc matplotlib
+conda install -c conda-forge/label/cf201901 matplotlib
+conda install -c anaconda pandaspactl -- set-sink-volume 0 150%
